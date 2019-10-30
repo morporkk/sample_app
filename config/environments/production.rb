5777883cf81603aedd5cb8649becb1edaf2cafc2
Miroslav <miroslavr@protonmail.com>
2019-10-30 23:54:52 +0100

    Add account activation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,9 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  host = 'https://boilingatoll.herokuapp.com/'
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
@@ -88,4 +90,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  
 end
